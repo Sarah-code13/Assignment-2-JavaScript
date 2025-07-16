cc4596d893a9bf2dec35e4c92f2aeb51ad47c525
Sarah Perdomo <sarahdanielap@gmail.com>
2025-07-16 14:47:33 -0400

    added image

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ class Smoothie {
         return `You ordered a smoothie with a size ${this.size} with the flavor of ${this.flavor} with the type of milk ${this.milk}, and requested ${addText}.`;
     }
 }
+document.querySelector(".smoothie-form").addEventListener("click", function (event) {
+    let size = document.querySelector(".size-cup").value;
+    let flavor = document.querySelector(".smoothie-flavor").value;
+    let milk = document.querySelector(".milk-type").value;
+    let addons = [];
+
+    for (let box of document.querySelectorAll(".addon:checked")) {
+        addons.push(box.value);
+    }
+    let order = new Smoothie(size, flavor, milk, addons);
+    output.textContent = order.description();
+
+    let smoothieImage = document.createElement("img");
+    smoothieImage.src = "images/smoothieCup.jpg";
+    smoothieImage.height = 150;
+    output.appendChild(smoothieImage);
+} );
